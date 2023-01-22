5c75f518b6d09acb7f188d73720674a526e625d5
b0h4x <ipereroi@myuax.com>
2023-01-22 22:16:12 +0100

    Added night mode (closes #6)

--- Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
+++ Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.feedback1;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    boolean isNight = false;
+    boolean nightModeEnabled;
+    SharedPreferences mSharedPreferences;
+    private static final String NIGHT_MODE = "NIGHT_MODE";
+    private static final String LANG = "LANGUAGE";
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Set correct NightMode (after inflation and onCreate
+        // which sets the corrects variables from SharedPreferences
+        setNightMode(nightModeEnabled, menu.findItem(R.id.night_mode));
         return true;
     }
 
@@ -23,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch (id) {
             case (R.id.night_mode):
-                isNight = !isNight;
+                nightModeEnabled = !nightModeEnabled;
+                SharedPreferences.Editor mSharedPreferencesEditor = mSharedPreferences.edit();
+                mSharedPreferencesEditor.putBoolean(NIGHT_MODE, nightModeEnabled);
+                mSharedPreferencesEditor.commit();
+                setNightMode(nightModeEnabled, item);
                 break;
             }
         return super.onOptionsItemSelected(item);
@@ -33,6 +48,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        // Get SharedPreferences for lang and nightmode
+        mSharedPreferences = getDefaultSharedPreferences(this);
+        nightModeEnabled = mSharedPreferences.getBoolean(NIGHT_MODE, false);
+        mSharedPreferences.getString(LANG, "en");
+    }
+
+    private void setNightMode(boolean nightModeEnabled, MenuItem menuItem) {
+        if (nightModeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            menuItem.setIcon(R.drawable.light_mode);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            menuItem.setIcon(R.drawable.night_mode);
+        }
     }
 }
