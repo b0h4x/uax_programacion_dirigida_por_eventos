b488a39d959efed662f5fe0c54d2fa31408507b8
b0h4x <ipereroi@myuax.com>
2023-01-22 18:33:02 +0100

    Added Toolbar menu (empty, closes #4)

--- Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
+++ Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.feedback1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
