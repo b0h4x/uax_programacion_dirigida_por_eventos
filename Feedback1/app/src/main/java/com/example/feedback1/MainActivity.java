3340361c79215adcd90545578d90a2d09c396292
b0h4x <ipereroi@myuax.com>
2023-01-22 19:23:13 +0100

    added night_mode icon for the menu

--- Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
+++ Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.feedback1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isNight = false;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -14,8 +19,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case (R.id.night_mode):
+                isNight = !isNight;
+                break;
+            }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 }
