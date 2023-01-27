746a372374a03b787e6f96417d0162f81ad30572
b0h4x <ipereroi@myuax.com>
2023-01-27 20:33:36 +0100

    Added multilang support (closes #5), minibug, preguntar a Ruben

--- Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
+++ Feedback1/app/src/main/java/com/example/feedback1/MainActivity.java
@@ -6,16 +6,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     boolean nightModeEnabled;
+    String language;
     SharedPreferences mSharedPreferences;
     private static final String NIGHT_MODE = "NIGHT_MODE";
     private static final String LANG = "LANGUAGE";
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         // Set correct NightMode (after inflation and onCreate
         // which sets the corrects variables from SharedPreferences
         setNightMode(nightModeEnabled, menu.findItem(R.id.night_mode));
+        setLang(language, menu.findItem(R.id.lang_mode));
         return true;
     }
 
@@ -40,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
                 mSharedPreferencesEditor.commit();
                 setNightMode(nightModeEnabled, item);
                 break;
+            case (R.id.lang_mode):
+                language = language=="en"?"es":"en";
+                mSharedPreferencesEditor = mSharedPreferences.edit();
+                mSharedPreferencesEditor.putString (LANG, language);
+                mSharedPreferencesEditor.commit();
+                setLang(language, item);
+
+               // setContentView(R.layout.activity_main);
+                restartActivity();
             }
         return super.onOptionsItemSelected(item);
     }
@@ -47,12 +60,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         // Get SharedPreferences for lang and nightmode
         mSharedPreferences = getDefaultSharedPreferences(this);
         nightModeEnabled = mSharedPreferences.getBoolean(NIGHT_MODE, false);
-        mSharedPreferences.getString(LANG, "en");
+        language = mSharedPreferences.getString(LANG, "en");
+
+        // Inflate
+        setContentView(R.layout.activity_main);
     }
 
     private void setNightMode(boolean nightModeEnabled, MenuItem menuItem) {
@@ -64,4 +79,24 @@ public class MainActivity extends AppCompatActivity {
             menuItem.setIcon(R.drawable.night_mode);
         }
     }
+
+    private void setLang(String language, MenuItem menuItem) {
+        if (language == "es") {
+            menuItem.setIcon(R.drawable.en);
+        } else if (language == "en") {
+            menuItem.setIcon(R.drawable.es);
+        }
+        // Set lang as default
+        Locale myLocale = new Locale(language);
+        Configuration config = new Configuration();
+        config.locale = myLocale;
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+    }
+
+    private void restartActivity() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
+
 }
