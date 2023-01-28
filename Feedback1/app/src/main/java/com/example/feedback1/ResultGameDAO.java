75c168fb44f8e7dda6c4e932bbbef6fe1db16df3
b0h4x <ipereroi@myuax.com>
2023-01-28 19:40:33 +0100

    Add DAO (closes #8)

---
+++ Feedback1/app/src/main/java/com/example/feedback1/ResultGameDAO.java
@@ -0,0 +1,22 @@
+package com.example.feedback1;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ResultGameDAO {
+    @Insert
+    void insert(int score);
+
+    // truncate table
+    @Query("DELETE FROM results_table")
+    void deleteAll();
+
+    // get the results
+    @Query("SELECT * FROM results_table")
+    LiveData<List<GameResult>> getAllResults();
+}
