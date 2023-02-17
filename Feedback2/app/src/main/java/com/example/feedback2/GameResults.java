4d589ed8f570fbf88beb829e4e8e8b555a308aa1
b0h4x <ipereroi@myuax.com>
2023-02-17 11:28:48 +0100

    El score del juego ahora actualiza la tabla de scores (closes #31)

--- Feedback2/app/src/main/java/com/example/feedback2/GameResults.java
+++ Feedback2/app/src/main/java/com/example/feedback2/GameResults.java
@@ -1,6 +1,7 @@
 package com.example.feedback2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class GameResults extends AppCompatActivity {
     int score;
     TextView mTextView;
     Button button;
+    GameResultViewModel mGameResultViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,10 @@ public class GameResults extends AppCompatActivity {
         mTextView = findViewById(R.id.score_result);
         mTextView.append(String.valueOf(score));
         button = findViewById(R.id.finish_game);
+
+        // Add score to db
+        mGameResultViewModel = ViewModelProviders.of(this).get(GameResultViewModel.class);
+        mGameResultViewModel.insert(score);
         button.setOnClickListener(
                 new View.OnClickListener() {
                   @Override
